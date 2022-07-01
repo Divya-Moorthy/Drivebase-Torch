6259ff18f3e1649b9aed05646720a6c0e08e11f9
Divya-Moorthy <96922089+Divya-Moorthy@users.noreply.github.com>
2022-06-30 19:29:43 -0700

    Test Function, correlation between current and speed

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -28,37 +28,90 @@ void Robot::RobotPeriodic() {
 void Robot::AutonomousInit() {} // first few seconds, creates variable/objects
 void Robot::AutonomousPeriodic() {} //repeats during autonomous period
 
-void Robot::TeleopInit() {} //Taxing init sequence
+void Robot::TeleopInit() {
+  
+} 
+
+//Taxing init sequence
 void Robot::TeleopPeriodic() { //periodic
-  float deadzone = 0.02;
-  double leftStick = l_Joystick->GetRawAxis(0);
-  double rightStick = r_Joystick->GetRawAxis(1);
-  double yMovement = leftStick - rightStick;
-  double xMovement = leftStick + rightStick;
-  int ySign = 1;
-  int xSign = 1;
-  double prcX, prcY;
+  deadzone = 0.02;
+//deadzone is margin of error
+
+  leftStick = l_Joystick->GetRawAxis(0);
+  rightStick = r_Joystick->GetRawAxis(1);
+//rawAxis is forward and backward
+// 0 on x, 1 on y 
+
+  yMovement = leftStick - rightStick;
+  xMovement = leftStick + rightStick;
+//pattern in table
+
+  ySign = 1;
+  xSign = 1;
+  
+// deadzone stuff
 
   prcY = fabs(leftStick);
   prcX = fabs(rightStick);
   
+  //checks to see if command could be from an error
+
   if (prcY <= deadzone) {
     
     rightLeadMotor->Set(0);
     leftLeadMotor->Set(0);
   } else {
-
+    //from the formula, slope from right side vs left
     rightLeadMotor->Set(ySign * (1/(1 - deadzone) * (yMovement) - (deadzone/(1 - deadzone)))); // left stick y-axis
+    //rightLeadMotor would take in y movement
     leftLeadMotor->Set(xSign * (1/(1 - deadzone) * (xMovement) - (deadzone/(1 - deadzone))));
-
+    //leftLeadMotor would take in x movement
   }
 }
 
 void Robot::DisabledInit() {} //closing sequence
 void Robot::DisabledPeriodic() {} // checking for closing command
 
-void Robot::TestInit() {} //teleop dedicated place to test
-void Robot::TestPeriodic() {} //same 
+void Robot::TestInit() {
+}
+ //teleop dedicated place to test
+void Robot::TestPeriodic() {
+ /*
+  for (double i=0; i <= 12; i += 0.5){
+    counter = 0;
+    if (counter == 10){
+      rightLeadMotor->SetVoltage(0);
+      leftLeadMotor->SetVoltage(0);
+    } else {
+      rightLeadMotor->SetVoltage(i);
+      leftLeadMotor->SetVoltage(i);
+      std::cout << "i value (0-12 by 0.5's) " << i << std::endl;
+      std::cout << "current at voltage of i " << rev::CANSparkMax::GetOutputCurrent() << std::endl;
+
+    }
+  }
+  */
+//This is supposed to find correlation between voltage and current
+//units did not match
+
+//This one is for current and speed
+
+ for (double i=-1.0; i <= 1.0; i += 0.5){
+    counter = 0;
+    if (counter == 10){
+      rightLeadMotor->Set(0);
+      leftLeadMotor->Set(0);
+    } else {
+      rightLeadMotor->Set(i);
+      leftLeadMotor->Set(i);
+      std::cout << "i value (-1 ti 1 by 0.5's) " << i << std::endl;
+      std::cout << "current at speed of i for right motor " << rightLeadMotor->GetOutputCurrent() << std::endl;
+      std::cout << "current at speed of i for left motor " << leftLeadMotor->GetOutputCurrent() << std::endl;
+      counter ++;
+    } 
+  }
+
+} //same 
 
 #ifndef RUNNING_FRC_TESTS
 int main() {
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -3,13 +3,20 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #pragma once
-
+//Only runs once
+//basically an init function
 #include <frc/TimedRobot.h>
 #include <rev/CANSparkMax.h>
 #include <frc/Joystick.h>
+#include <iostream>
+//#include <units.h>
+//#include <units/voltage.h>
 
+//class robot, established in robot.cpp
 class Robot : public frc::TimedRobot {
  public:
+  
+  //all functions also in Robot.cpp
   void RobotInit() override;
   void RobotPeriodic() override;
 
@@ -28,13 +35,26 @@ class Robot : public frc::TimedRobot {
   int rightLeadMotorID = 12;
   int leftLeadMotorID = 3;
 
-  // lMotor = 3;
-  // rMotor = 12;
+  float deadzone;
+  double leftStick;
+  double rightStick;
+  int ySign, xSign;
+  double prcX, prcY;
+  double yMovement, xMovement;
+
+  int counter;
+  
+//motor id's 
+//you can change them
+
+  
   
   //Joysticks 
   frc::Joystick * l_Joystick = new frc::Joystick(0); // assign to port 0
-  frc::Joystick * r_Joystick = new frc::Joystick(1); // assign to port 0
+  frc::Joystick * r_Joystick = new frc::Joystick(1); // assign to port 1
+//port is left or right joystick
 
+//library rev, init function/class CANSparkMax, rightLeadMotor is a variable storing the class CANSparkMax
 
   rev::CANSparkMax *rightLeadMotor = new rev::CANSparkMax(rightLeadMotorID, rev::CANSparkMax::CANSparkMaxLowLevel::MotorType::kBrushless);
   rev::CANSparkMax *leftLeadMotor = new rev::CANSparkMax(leftLeadMotorID, rev::CANSparkMax::CANSparkMaxLowLevel::MotorType::kBrushless);
